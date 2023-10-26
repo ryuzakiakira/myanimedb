0c0e5e33766d186558c495a61155cc2d5b9593ae
Tushar Chauhan <kunyukki16@gmail.com>
2023-10-26 22:10:24 +0530

    fixed a typo in the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ import AnimeList from "../components/AnimeList";
 function HomePage() {
   const [seasonalAnime, setSeasonalAnime] = useState([]);
 
-  const getTopAnime = async () => {
+  const getAnime = async () => {
     const temp = await fetch("https://api.jikan.moe/v4/seasons/now");
     const anime = await temp.json();
     setSeasonalAnime(anime.data);
   };
 
   useEffect(() => {
-    getTopAnime();
+    getAnime();
   }, []);
 
   return (
