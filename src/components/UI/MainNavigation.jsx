1e63417e03aa12b0444f3427f0b22b62cc87c86e
Tushar Chauhan <ryuzakiakira@protonmail.com>
2023-11-04 16:44:29 +0530

    made navbar work a little better still broken though

--- src/components/UI/MainNavigation.jsx
+++ src/components/UI/MainNavigation.jsx
@@ -4,18 +4,23 @@ import classes from "./MainNavigation.module.css";
 import { useEffect, useState } from "react";
 
 function MainNavigation() {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [fetchedData, setFetchedData] = useState(null);
+  const [menuVisible, setMenuVisible] = useState(false);
   const navigate = useNavigate();
 
   const searchInputHandler = (e) => {
     setSearch(e.target.value);
-  }
+  };
 
   const getSearch = async () => {
     try {
-      const temp1 = await fetch(`https://api.jikan.moe/v4/manga?q=${search}&order_by=rank&sort=asc&limit=20`);
-      const temp2 = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&order_by=rank&sort=asc&limit=20`);
+      const temp1 = await fetch(
+        `https://api.jikan.moe/v4/manga?q=${search}&order_by=rank&sort=asc&limit=20`
+      );
+      const temp2 = await fetch(
+        `https://api.jikan.moe/v4/anime?q=${search}&order_by=rank&sort=asc&limit=20`
+      );
       const manga = await temp1.json();
       const anime = await temp2.json();
       const searchResults = [...anime.data, ...manga.data];
@@ -23,24 +28,43 @@ function MainNavigation() {
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-  }
+  };
 
   const sendData = () => {
-    navigate("/results", { state: { data: fetchedData } })
-  }
+    navigate("/results", { state: { data: fetchedData } });
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
     getSearch();
-  }
+    setMenuVisible(false);
+  };
 
   useEffect(() => {
     if (fetchedData) {
       sendData();
     }
-  }, [fetchedData])
+  }, [fetchedData]);
 
-  const hideNavBar = () => { }
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+    console.log(menuVisible);
+  };
+
+  const searchField = (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="search">
+        <input
+          type="search"
+          id="search"
+          name="search"
+          placeholder="Search..."
+          value={search}
+          onChange={searchInputHandler}
+        />
+      </label>
+    </form>
+  );
 
   return (
     <header>
@@ -90,20 +114,31 @@ function MainNavigation() {
           </ul>
         </div>
         <div className={classes.right}>
-          <form onSubmit={submitHandler}>
-            <label htmlFor="search">
-              <input
-                type="search"
-                id="search"
-                name="search"
-                placeholder="Search..."
-                value={search}
-                onChange={searchInputHandler}
-              />
-            </label>
-          </form>
+          <a
+            href="#"
+            className={`${classes.icon} ${menuVisible ? classes.active : ""}`}
+            onClick={toggleMenu}
+          >
+            &#9776;
+          </a>
+          <div className={classes.searchField}>{searchField}</div>
         </div>
-        <a href="#" className={classes.icon} onClick={hideNavBar}>&#9776;</a>
+        {menuVisible && (
+          <div className={classes.dropdown}>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/anime">Anime</a>
+              </li>
+              <li>
+                <a href="/manga">Manga</a>
+              </li>
+              <li>{searchField}</li>
+            </ul>
+          </div>
+        )}
       </nav>
     </header>
   );
